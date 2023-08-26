b62d0693924dd60eef6158a530c3f206915f8ea9
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-08-26 22:46:00 +0200

    update project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule
