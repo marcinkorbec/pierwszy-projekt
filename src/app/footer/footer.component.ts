6e6479257d7d35c21ba71ec6f5def56794bf41be
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-08-31 13:54:21 +0200

    inline component, tworzenie footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   template: `
-    <p class="text-center">
+    <p class="text-center container">
       &copy; 2023 Marcin Korbecki
     </p>
   `,
