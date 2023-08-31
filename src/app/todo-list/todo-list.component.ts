bdfe233ae70999f4440219d539861ffa520dcde2
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-08-31 17:17:15 +0200

    dyrektywa ngFor i tworzenie interfejsu Todo

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
+import {Todo} from "../shared/interfaces/todo.interface";
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
+
 export class TodoListComponent {
-  todos: string[] = [];
+  todos: Todo[] = [];
 
   addTodo(todo: string): void {
     if (todo.length <= 3) {
       alert('Zadanie musi mieć conajmniej 4 znaki!')
       return;
     }
-    this.todos.push(todo);
+    this.todos.push({name: todo, isComplete: false});
     console.log('Aktualna lista todo', this.todos)
   }
 }
